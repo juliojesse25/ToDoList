329847b63a7e64e227b04c9fac3c9ad8a26b5c27
juliojesse25 <juliojesse25@gmail.com>
2019-07-24 15:28:11 -0400

    Add Components folder in src folder

---
+++ src/Components/MyApp.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+function MyApp() {
+  return (
+    <div>
+      <h1>Jesse Kasumba</h1>
+      <p>
+        A Full Stack Web Developer currently working on honing my React skills.
+        My top 3 vacation spots are;
+      </p>
+      <ol>
+        <li>Kampala, Uganda</li>
+        <li>London, United Kingdom</li>
+        <li>Nairobi, Kenya</li>
+      </ol>
+    </div>
+  )
+}
+
+export default MyApp
